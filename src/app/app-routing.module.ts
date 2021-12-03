968d0c871af18fe2660c5f1463fa323d6a78525d
Pablo <pablofats@hotmail.com>
2021-12-03 15:32:07 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProdutosComponent} from "./produtos/produtos.component";
+import {AdicionarProdutosComponent} from "./produtos/adicionar-produtos/adicionar-produtos.component";
+import {ListarProdutosComponent} from "./produtos/listar-produtos/listar-produtos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produtos',
+    component: ProdutosComponent,
+    children: [
+      {
+        path: 'adicionar',
+        component: AdicionarProdutosComponent,
+      },
+      {
+        path: 'listar',
+        component: ListarProdutosComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { NavComponent } from './view/nav/nav.component';
+import { AdicionarProdutosComponent } from './produtos/adicionar-produtos/adicionar-produtos.component';
+import { ListarProdutosComponent } from './produtos/listar-produtos/listar-produtos.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent,
+    NavComponent,
+    AdicionarProdutosComponent,
+    ListarProdutosComponent,
+
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
