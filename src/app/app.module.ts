db832aa0563fea6b734c624a200c99f32bd35804
Pablo <pablofats@hotmail.com>
2021-12-27 13:23:20 -0300

    Autenticação com auth0 e alguns ajustes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { ListarUsuariosComponent } from './usuarios/listar-usuarios/listar-usuarios.component';
 import { CadastrarUsuariosComponent } from './usuarios/cadastrar-usuarios/cadastrar-usuarios.component';
 import { NgxMaskModule, IConfig } from 'ngx-mask'
+import { AuthModule } from '@auth0/auth0-angular';
+import { AppAuthButtonComponent } from './auth/app-auth-button/app-auth-button.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { NgxMaskModule, IConfig } from 'ngx-mask'
     UsuariosComponent,
     ListarUsuariosComponent,
     CadastrarUsuariosComponent,
+    AppAuthButtonComponent,
 
   ],
     imports: [
@@ -32,7 +35,11 @@ import { NgxMaskModule, IConfig } from 'ngx-mask'
         FormsModule,
         ReactiveFormsModule,
         HttpClientModule,
-        NgxMaskModule.forRoot()
+        NgxMaskModule.forRoot(),
+        AuthModule.forRoot({
+          domain: "dev-feczwkc9.us.auth0.com",
+          clientId: "Y36HoI9oWOu6IkYXbRXzNhpSkFAwiObQ"
+        }),
     ],
   providers: [],
   bootstrap: [AppComponent]
